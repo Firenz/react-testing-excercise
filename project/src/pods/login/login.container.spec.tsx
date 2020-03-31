8c0f51e4fcf5e90fb749a2376ae04f85753562ab
Alicia G <alicia.guardenoalbertos@gmail.com>
2020-03-31 16:30:22 +0200

    update login.container.spec

--- project/src/pods/login/login.container.spec.tsx
+++ project/src/pods/login/login.container.spec.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react';
 import { HashRouter, Switch, Route } from 'react-router-dom';
-import { render, waitForElement } from '@testing-library/react';
+import {
+  render,
+  waitForElement,
+  fireEvent,
+  wait,
+} from '@testing-library/react';
 import * as reactRouterDom from 'react-router-dom';
 import * as api from './login.api';
 import * as core from 'core';
 import * as LoginVm from './login.vm';
+import { SessionProvider } from 'core';
 import { HotelCollectionScene } from 'scenes';
 import { LoginContainer } from './login.container';
 
@@ -22,14 +28,27 @@ const renderWithRouter = component => {
 };
 
 describe('login container specs', () => {
-  xit('should render hotel collection component when login is succesfull', async () => {
+  it('should render hotel collection component when login is succesfull', async () => {
     //Arrange
+    const loginStub = jest.spyOn(LoginVm, 'createEmptyLogin').mockReturnValue({
+      login: 'admin',
+      password: 'test',
+    });
     const getValidateCredentialsStub = jest
       .spyOn(api, 'validateCredentials')
       .mockResolvedValue(true);
 
     //Act
-    const { getByTestId } = renderWithRouter(<LoginContainer />);
+    const { getByTestId } = renderWithRouter(
+      <SessionProvider>
+        <LoginContainer />
+      </SessionProvider>
+    );
+    const buttonElement = getByTestId('login-button');
+
+    await wait(() => {
+      fireEvent.click(buttonElement);
+    });
     // const hotelCollectionElement = await waitForElement(() => getByTestId('hotel-collection'));
 
     //Assert
